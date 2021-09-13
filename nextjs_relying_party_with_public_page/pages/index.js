b78264f3e69ddc9e592b116f351f10f7534dd9d2
thinkAmi <dev.thinkami+git@gmail.com>
2021-09-13 23:15:42 +0900

    replace from Link component to a tag

--- nextjs_relying_party_with_public_page/pages/index.js
+++ nextjs_relying_party_with_public_page/pages/index.js
@@ -1,12 +1,25 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 
-export default function Home() {
+export const getServerSideProps = async function ({ req, res }) {
+  return {
+    props: {
+      host: process.env.NEXT_HOST,
+      port: process.env.PORT,
+    }
+  }
+}
+
+
+export default function Home({ host, port }) {
   return (
     <>
       <h1>Index page</h1>
-      <Link href="/profile" passHref>
-        <button>Go Profile</button>
-      </Link>
+
+      {/* クライアントでのルーティングができないので、aタグに差し替え */}
+      <a href={`${host}:${port}/profile`}>Go</a>
+      {/*<Link href="/profile" passHref>*/}
+      {/*  <button>Go Profile</button>*/}
+      {/*</Link>*/}
     </>
   )
 }
